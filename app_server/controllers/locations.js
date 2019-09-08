f9a30250733e3966a508cb6574d39ad648f1a3ba
unknown <>
2019-09-08 11:38:20 -0400

    Updated locations list

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -9,7 +9,15 @@ const locationInfo = (req, res) => {
 /* GET 'Add review' page */
 const homelist = (req, res) => {
     res.render('locations-list', { title: 'Home' });
-};
+  };
+  
+  const locationInfo = (req, res) => {
+    res.render('location-info', { title: 'Location Info' });
+  };
+  
+  const addReview = (req, res) => {
+    res.render('location-review-form', { title: 'Add review' });
+  };
 module.exports = {
     homelist,
     locationInfo,
